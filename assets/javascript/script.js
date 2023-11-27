7b2376528e521888a580724f7b0cc3480ff90d12
Seven <phoenixpyra7@gmail.com>
2023-11-27 11:56:16 -0600

    commented out the fetch for now

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,19 +7,15 @@ var queryURL = "https://home.openweathermap.org/api_keys"
 
 
 
+//moved the call into the fetch api. might need to make new call.
 
 
 
-
-
-
-
-
-fetch (api.openweathermap.org/data/2.5/weather?q=city&appid="https://home.openweathermap.org/api_keys/45943224f1c4d4f4cace2f1863924b15")
-    .then(function(response) {
-        return response.json.apply();
-    })
-    .then(function(data) {
-        console.log(data);
-        //Do something with the data here
-    });
+// fetch (api.openweathermap.org/data/2.5/weather?q=city&appid="https://home.openweathermap.org/api_keys/45943224f1c4d4f4cace2f1863924b15")
+//     .then(function(response) { //need to add state above
+//         return response.json.apply();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+//         //Do something with the data here
+//     });
