c0f7c64e5f4aff3e2a200ad9f639298d0b8669a8
Seven <phoenixpyra7@gmail.com>
2023-11-27 12:52:23 -0600

    added to broken fetch code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,11 +11,15 @@ var queryURL = "https://home.openweathermap.org/api_keys"
 
 
 
-// fetch (api.openweathermap.org/data/2.5/weather?q=city&appid="https://home.openweathermap.org/api_keys/45943224f1c4d4f4cace2f1863924b15")
-//     .then(function(response) { //need to add state above
+// fetch(api.openweathermap.org / data / 2.5 / weather ? q = city & appid="https://home.openweathermap.org/api_keys/45943224f1c4d4f4cace2f1863924b15")
+//     .then(function (response) { //need to add state above
 //         return response.json.apply();
 //     })
-//     .then(function(data) {
+//     .then(function (data) {
 //         console.log(data);
-//         //Do something with the data here
-//     });
+//         for (var i = 0; i < DataView; i++) {
+//             var city = document.getElementsByClassName("");
+//             var stateCode = document.getElementsByClassName("");
+//             weatherContainer.append(city, stateCode)
+//             //         //Do something with the data here
+//         });
