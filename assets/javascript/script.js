9c28a72fdd899c6634ff21ecf61ce841d9cb32ba
Seven <phoenixpyra7@gmail.com>
2023-11-27 11:49:25 -0600

    added weather api link and key, but it is causing an error.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,8 @@
 
 var APIKey = "45943224f1c4d4f4cace2f1863924b15";
+var city;
+var stateCode;
+var queryURL = "https://home.openweathermap.org/api_keys"
 
 
 
@@ -11,7 +14,8 @@ var APIKey = "45943224f1c4d4f4cace2f1863924b15";
 
 
 
-fetch ("")
+
+fetch (api.openweathermap.org/data/2.5/weather?q=city&appid="https://home.openweathermap.org/api_keys/45943224f1c4d4f4cace2f1863924b15")
     .then(function(response) {
         return response.json.apply();
     })
