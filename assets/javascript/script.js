44d735644ebfa76ff150e345f6c4c858c96496e4
Seven <phoenixpyra7@gmail.com>
2023-11-29 21:22:32 -0600

    fixed fetch button, pulls city info correctly.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,14 @@
 
 var APIKey = "45943224f1c4d4f4cace2f1863924b15";
-var city = "Chicago";
-var stateCode;
+// var city = "";
 var queryURL = "https://home.openweathermap.org/api_keys";
 var clearButton = document.getElementById("clear-button");
+var fetchButton = document.getElementById("fetch-button");
 var results = document.querySelector(".results");
 var weatherContainer;
+var qInput = document.querySelector("#q");
+
+
 // var = "#loc-search"; //made an id and that aint working
 
 // api for geo location
@@ -19,13 +22,15 @@ function getGeoWeather(lat,lon) {
         console.log(data.list[0].main.temp);
         var todaysWeather = data.list[0].main.temp;
         var todaysWeatherP = document.createElement("p");
-        todaysWeatherP.textContent = todaysWeather
-        // append to the results
+        todaysWeatherP.textContent = todaysWeather;
+        results.appendChild(todaysWeatherP);
+
     })
 };
 // api for weather
 function getCityGeoData(city) {
-    var city = "Chicago";
+    var city = qInput.value;
+    console.log(city)
     fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=45943224f1c4d4f4cace2f1863924b15")
         .then(function (response) {
             return response.json();
@@ -38,38 +43,11 @@ function getCityGeoData(city) {
         })
 }
 
-getCityGeoData();
+// getCityGeoData();
 
+fetchButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    getCityGeoData();
+}); 
+// clearButton.addEventListener("click", clear);
 
-
-function weatherSearch(event) {
-    event.preventDefaul();
-    var format = formatInput.value;
-    var q = qInput.value;
-    var type = "search";
-    if (format) {
-        type = format;
-    }
-    var url = " " + type + '/?q' + q + "&for+json";
-
-    fetch(url)
-    .then(function (response) {
-        return response.json();
-    })
-}
-//         for (var i = 0; i < DataView; i++) {
-//             var city = document.getElementsByClassName("");
-//             var stateCode = document.getElementsByClassName("");
-//             weatherContainer.append(city,stateCode)
-//          
-//Do something with the data here
-//});
-
-// function clear() {
-//     weatherContainer.innerHTML = null; //make sure class in html or move
-// }
-
-//why is my get api not green
-fetchButton.addEventListener("click", getApi); 
-clearButton.addEventListener("click", clear);
-fetchButton.addEventListener("click", getApi);
