6cd2d72c53980ee52f1f4bf7541fa2241d8f4495
Seven <phoenixpyra7@gmail.com>
2023-12-02 16:37:13 -0600

    attempt to do local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,45 +9,48 @@ var weatherContainer;
 var qInput = document.querySelector("#q");
 
 
-// var = "#loc-search"; //made an id and that aint working
-
-// api for geo location
-function getGeoWeather(lat,lon) {
+// api for the geo location
+function getGeoWeather(lat, lon) {
     fetch("https://api.openweathermap.org/data/2.5/forecast?appid=45943224f1c4d4f4cace2f1863924b15&lat=" + lat + "&lon=" + lon + "&units=imperial")
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        console.log(data.list[0].main.temp);
-        var todaysWeather = data.list[0].main.temp;
-        var todaysWeatherP = document.createElement("p");
-        todaysWeatherP.textContent = todaysWeather;
-        results.appendChild(todaysWeatherP);
-
-    })
-};
-// api for weather
-function getCityGeoData(city) {
-    var city = qInput.value;
-    console.log(city)
-    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=45943224f1c4d4f4cace2f1863924b15")
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
+            console.log(data.list[0].main.temp);
+            var todaysWeather = data.list[0].main.temp;
+            var todaysWeatherP = document.createElement("p");
+            todaysWeatherP.textContent = todaysWeather;
+            results.appendChild(todaysWeatherP);
+            // add more then the temp to this section
+        })
+};
+// api for weather
+function getCityGeoData(city) {
+    var city = qInput.value;
+    // console.log(city)
+    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=45943224f1c4d4f4cace2f1863924b15")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // console.log(data);
             var lat = data[0].lat;
             var lon = data[0].lon;
             getGeoWeather(lat, lon);
+            localStorage.setItem(city, data);
         })
 }
-
+// function getCityWeatherForcast {
+//     loadForcastFromLocalStorage(city, data);
+//     console.log()
+// }
 // getCityGeoData();
+// this code stopped the temp from appearing on the page
 
-fetchButton.addEventListener("click", function(event) {
+fetchButton.addEventListener("click", function (event) {
     event.preventDefault();
     getCityGeoData();
-}); 
+});
 // clearButton.addEventListener("click", clear);
 
